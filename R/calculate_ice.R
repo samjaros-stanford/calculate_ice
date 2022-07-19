bdf6292c7ed217b84b152467c6cf411fa2c147eb
samjaros-stanford <76895253+samjaros-stanford@users.noreply.github.com>
2022-07-19 11:34:57 -0700

    Initial commit with renv and census API key

---
+++ R/calculate_ice.R
@@ -0,0 +1,10 @@
+#' @author Hoda Abdel Magid
+#' @author Sam Jaros
+#' @name calculate_ice
+#' @import tidycensus
+
+library(tidycensus)
+
+census_api_key("1f910d0c1dae50841ad164e77baa3c2c651937c0", install=T)
+
+
